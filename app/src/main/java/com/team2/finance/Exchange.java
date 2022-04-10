4d5b824c242d49e9dd29f1c5df2ffb89871bded0
Alon <alon.ttp@gmail.com>
2022-04-10 19:45:48 +0300

    Update Exchange.java

--- app/src/main/java/com/team2/finance/Exchange.java
+++ app/src/main/java/com/team2/finance/Exchange.java
@@ -1,51 +1,39 @@
 package com.team2.finance;
 
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.JsonRequest;
-import com.squareup.okhttp.OkHttpClient;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Iterator;
-import java.util.List;
 
+public class Exchange extends BaseActivity {
 
-public class Exchange extends AppCompatActivity {
-
-    EditText fromCurrency,toCurrency;
+    EditText fromCurrency, toCurrency;
     Spinner fromDropdown, toDropdown;
     Button convert_bt;
     private RequestQueue requestQueue;
     private int user_type = 1;
+    private static final String TAG = "Exchange";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exchange);
+        View rootView = getLayoutInflater().inflate(R.layout.activity_exchange, frameLayout);
 
         convert_bt = (Button) findViewById(R.id.convert_bt);
         fromCurrency = findViewById(R.id.fromCurrency);
@@ -61,34 +49,34 @@ public class Exchange extends AppCompatActivity {
 
         convert_bt.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Convert();
+                if (Validation.isEmpty(fromCurrency.getText().toString())) {
+                    Convert();
+                } else {
+                    fromCurrency.setError("Empty value");
+                }
             }
         });
-
     }
 
     private void initSpinners() throws JSONException {
-        if (user_type == 1){
+        if (user_type == 1) {
             getCurrency();
-        }
-        else{
-            ArrayList<String> foreign_currency =new ArrayList<>(Arrays.asList("USD", "ILS", "EUR"));
+        } else {
+            ArrayList<String> foreign_currency = new ArrayList<>(Arrays.asList("USD", "ILS", "EUR"));
             ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                     android.R.layout.simple_spinner_item, foreign_currency);
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             fromDropdown.setAdapter(adapter);
             toDropdown.setAdapter(adapter);
         }
-
     }
 
     private void Convert() {
 
-        if (fromDropdown.getSelectedItem().toString().equals(toDropdown.getSelectedItem().toString())){
+        if (fromDropdown.getSelectedItem().toString().equals(toDropdown.getSelectedItem().toString())) {
             toCurrency.setText(fromCurrency.getText().toString());
-        }
-        else{
-            String url="https://frankfurter.app/latest?amount=" + fromCurrency.getText().toString() + "&from=" + fromDropdown.getSelectedItem().toString() + "&to=" + toDropdown.getSelectedItem().toString();
+        } else {
+            String url = "https://frankfurter.app/latest?amount=" + fromCurrency.getText().toString() + "&from=" + fromDropdown.getSelectedItem().toString() + "&to=" + toDropdown.getSelectedItem().toString();
 
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                     (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
@@ -112,16 +100,15 @@ public class Exchange extends AppCompatActivity {
 
                         @Override
                         public void onErrorResponse(VolleyError error) {
-                            // TODO: Handle error
+                            Log.e(TAG, String.valueOf(error));
                         }
                     });
             requestQueue.add(jsonObjectRequest);
         }
-
     }
 
     private void getCurrency() throws JSONException {
-        String url ="https://frankfurter.app/latest";
+        String url = "https://frankfurter.app/latest";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                     @Override
@@ -135,11 +122,11 @@ public class Exchange extends AppCompatActivity {
 
                         Iterator keysToCopyIterator = jsonObject.keys();
                         ArrayList<String> keysList = new ArrayList<String>();
-                        while(keysToCopyIterator.hasNext()) {
+                        while (keysToCopyIterator.hasNext()) {
                             String key = (String) keysToCopyIterator.next();
                             keysList.add(key);
                         }
-                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Exchange.this,android.R.layout.simple_spinner_item, (ArrayList<String>) keysList);
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Exchange.this, android.R.layout.simple_spinner_item, (ArrayList<String>) keysList);
                         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                         fromDropdown.setAdapter(adapter);
                         toDropdown.setAdapter(adapter);
@@ -149,11 +136,9 @@ public class Exchange extends AppCompatActivity {
 
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        // TODO: Handle error
+                        Log.e(TAG, String.valueOf(error));
                     }
                 });
         requestQueue.add(jsonObjectRequest);
     }
-
-
 }
