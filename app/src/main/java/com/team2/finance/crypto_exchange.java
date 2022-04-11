0cb919d920c72e340f54011a9a3a7d0dee796e5d
toliko <tkcoding01@gmail.com>
2022-04-11 19:57:40 +0300

    BSPM2022T2-57 - Convert Coins using API

--- app/src/main/java/com/team2/finance/crypto_exchange.java
+++ app/src/main/java/com/team2/finance/crypto_exchange.java
@@ -56,7 +56,8 @@ public class crypto_exchange extends AppCompatActivity {
         toCurrency = findViewById(R.id.toCurrency);
         fromDropdown = findViewById(R.id.fromDropdown);
         toDropdown = findViewById(R.id.toDropdown);
-        result = findViewById(R.id.result);
+
+        //result = findViewById(R.id.result);
 
         requestQueue = VolleySingleton.getInstance(this).getRequestQueue();
 
@@ -84,7 +85,8 @@ public class crypto_exchange extends AppCompatActivity {
     /// function that will convert the chosen crypto coin
     private void Convert()
     {
-        String url = "https://api.coinstats.app/public/v1/coins?skip=0&limit=10&currency=USD";
+
+        String url = "https://api.coinstats.app/public/v1/coins?skip=0&limit=20&currency=USD";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                     @Override
@@ -92,16 +94,45 @@ public class crypto_exchange extends AppCompatActivity {
                         try {
                             JSONArray jsonArray = response.getJSONArray("coins");
 
+                            float frompirce = 1;
+                            float toprice=1;
+                            float result=1;
+
                             for(int i = 0 ; i < jsonArray.length() ; i++)
                             {
                                 JSONObject coin = jsonArray.getJSONObject(i);
-                                String type = coin.getString("name");
-                                String price = coin.getString("price");
-                                result.append(type + price + "\n");
+                                String type = coin.getString("symbol");
+                                if (fromDropdown.getSelectedItem().toString().equals(type))
+                                {
+                                    //check that the field isnt empty ( if it does put 1)
+                                    if(fromCurrency.getText().toString().equals("")) {
+                                        frompirce = Float.parseFloat(coin.getString("price"));
+                                        fromCurrency.setText("1");
+                                    }
+                                    else
+                                    {
+                                        frompirce = Float.parseFloat(coin.getString("price")) * Float.parseFloat(fromCurrency.getText().toString());
+                                    }
+
+                                }
+
+                                if (toDropdown.getSelectedItem().toString().equals(type))
+                                {
+                                    toprice = Float.parseFloat(coin.getString("price"));
+                                }
+
+                                //Show the exchange price on the field
+                                result = frompirce/toprice;
+                                toCurrency.setText(String.valueOf(result));
+
                             }
+
+
                         } catch (JSONException e) {
                             e.printStackTrace();
+                            Log.e("Conver Eroor","Somthing went Wrong on the onResponse Function");
                         }
+
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -113,23 +144,23 @@ public class crypto_exchange extends AppCompatActivity {
 
         requestQueue.add(jsonObjectRequest);
 
-
     }
 
     //function that will initiate the spinners
     private void initSpinners() throws JSONException
     {
         //array of all the Crypto symbols to display at the spinners
-        ArrayList<String> coins_symbol = new ArrayList<String>();
+        ArrayList<String> coins_symbol = new ArrayList<>();
 
-        String url = "https://api.coinstats.app/public/v1/coins?skip=0&limit=15&currency=USD";
+        String url = "https://api.coinstats.app/public/v1/coins?skip=0&limit=20&currency=USD";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                 (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
+                        //create array of all the objects innside coins Jsonfile
+                        JSONArray jsonArray = null;
                         try {
-                            JSONArray jsonArray = response.getJSONArray("coins");
-
+                            jsonArray = response.getJSONArray("coins");
 
                             for(int i = 0 ; i < jsonArray.length() ; i++)
                             {
@@ -140,9 +171,24 @@ public class crypto_exchange extends AppCompatActivity {
 
                             }
 
+
+
+
                         } catch (JSONException e) {
                             e.printStackTrace();
+                            Log.e("initSpinners","catch inside onResponse function");
                         }
+
+                        //add to the spinners
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(crypto_exchange.this,
+                                android.R.layout.simple_spinner_item,
+                                (ArrayList<String>) coins_symbol);
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        fromDropdown.setAdapter(adapter);
+                        toDropdown.setAdapter(adapter);
+//
+
+
                     }
                 }, new Response.ErrorListener() {
                     @Override
@@ -153,14 +199,6 @@ public class crypto_exchange extends AppCompatActivity {
 
 
         requestQueue.add(jsonObjectRequest);
-
-
-        //add to the spinners
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, coins_symbol);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        fromDropdown.setAdapter(adapter);
-        toDropdown.setAdapter(adapter);
 
     }
 
