5d80fbdf75f86163be8078077906ef9bcb8d9218
toliko <tkcoding01@gmail.com>
2022-04-12 03:39:41 +0300

    BSPM2022T2-60 - GUI for the Articels Cards

---
+++ app/src/main/java/com/team2/finance/crypto_articles.java
@@ -0,0 +1,19 @@
+package com.team2.finance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+public class crypto_articles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crypto_articles);
+
+        //TextView Article1_link = (TextView) findViewById(R.id.article1);
+        //Article1_link.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+}
