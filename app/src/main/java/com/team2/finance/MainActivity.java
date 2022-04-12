91b3c353169a1f86f008b49d6b92b367deb25415
Alon Teplitsky <alon.ttp@gmail.com>
2022-04-12 16:45:27 +0300

    Revert "Delete MainActivity.java"
    This reverts commit 98035828684ce6067579f8c74cd3c51f94f07acd.

---
+++ app/src/main/java/com/team2/finance/MainActivity.java
@@ -0,0 +1,29 @@
+package com.team2.finance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        //go to crypto exchange activity
+        Intent goToNextActivity = new Intent(getApplicationContext(), crypto_articles.class);
+        startActivity(goToNextActivity);
+
+
+    }
+}
