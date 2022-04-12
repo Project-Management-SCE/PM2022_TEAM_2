23a47aec30344a73f2681f5ff958336cd253a457
Alon Teplitsky <alon.ttp@gmail.com>
2022-04-12 16:49:42 +0300

    Revert changes

--- app/src/main/java/com/team2/finance/crypto_articles.java
+++ app/src/main/java/com/team2/finance/crypto_articles.java
@@ -13,16 +13,7 @@ public class crypto_articles extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crypto_articles);
 
-        TextView Article1_link = (TextView) findViewById(R.id.link_1);
-        Article1_link.setMovementMethod(LinkMovementMethod.getInstance());
-
-        TextView Article2_link = (TextView) findViewById(R.id.link_2);
-        Article2_link.setMovementMethod(LinkMovementMethod.getInstance());
-
-        TextView Article3_link = (TextView) findViewById(R.id.link_3);
-        Article3_link.setMovementMethod(LinkMovementMethod.getInstance());
-
-        TextView Article4_link = (TextView) findViewById(R.id.link_4);
-        Article4_link.setMovementMethod(LinkMovementMethod.getInstance());
+        //TextView Article1_link = (TextView) findViewById(R.id.article1);
+        //Article1_link.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
