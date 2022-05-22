8ffa8568f3de022438886fc8d9fba067e5bca975
SagiBiran <sagibit@gmail.com>
2022-05-22 18:11:26 +0300

    BSPM2022T2-107, BSPM2022T2-108 About "App Finance" Information Page - Done.
    BSPM2022T2-100, BSPM2022T2-100 Finance Information Page Done.
    09b367c
    In Addition to last belonging commit these are the new updates:
    
    -About Page UI Design Fix
    -Scroll Layout Additions
    -Data Insertions and information.

--- app/src/main/java/com/team2/finance/AboutActivity.java
+++ app/src/main/java/com/team2/finance/AboutActivity.java
@@ -1,5 +1,6 @@
 package com.team2.finance;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -10,7 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.ImageButton;
 
+import com.team2.finance.Pages.HomeActivity;
 import com.team2.finance.databinding.ActivityAboutBinding;
 
 public class AboutActivity extends AppCompatActivity {
@@ -20,22 +23,25 @@ public class AboutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityAboutBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
 
         Toolbar toolbar = binding.toolbar;
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
         toolBarLayout.setTitle(getTitle());
 
-        FloatingActionButton fab = binding.fab;
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        ImageButton back = binding.back;
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
             }
         });
+
+
     }
 }
